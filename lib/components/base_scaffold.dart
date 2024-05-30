23cc2953c85ddacdb3d6913f6d4b47dd5afa05b8
Ali Mazhar <alimazharsultan@gmail.com>
2024-05-30 16:16:01 +0500

    add zoom feature

--- lib/components/base_scaffold.dart
+++ lib/components/base_scaffold.dart
@@ -6,8 +6,6 @@ import 'package:mhgap_urdu/utils/home_screen_list.dart';
 import 'package:mhgap_urdu/components/arrow_navigation_container.dart';
 import 'package:mhgap_urdu/utils/texts.dart';
 
-
-
 class BaseScreen extends StatelessWidget {
   final Widget body;
   final String titleText;
@@ -33,6 +31,7 @@ class BaseScreen extends StatelessWidget {
     );
   }
 }
+
 class BaseScaffold extends StatelessWidget {
   final Widget body;
   final Widget? bottomNavigationBar;
@@ -57,15 +56,19 @@ class BaseScaffold extends StatelessWidget {
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.only(top: 50.0),
-            child: body,
+            child: InteractiveViewer(
+                panEnabled: true, // Enables panning
+                minScale: 0.5, // Minimum scale factor for zoom-out
+                maxScale: 4.0, // Maximum scale factor for zoom-in
+                child: body),
           ),
           if (showOrangeBar)
             Positioned(
               top: 0,
               left: 0,
               right: 0,
-              child: ArrowNavigationContainer(
-                  text: title, showBackButton: true),
+              child:
+                  ArrowNavigationContainer(text: title, showBackButton: true),
             ),
         ],
       ),
@@ -73,74 +76,3 @@ class BaseScaffold extends StatelessWidget {
     );
   }
 }
-
-class HomeScreenScaffold extends StatelessWidget {
-  final Widget body;
-  final Widget? bottomNavigationBar;
-  final String title;
-
-  const HomeScreenScaffold({
-    super.key,
-    required this.body,
-    this.bottomNavigationBar,
-    required this.title,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-
-    return Scaffold(
-        key: scaffoldKey,
-        appBar: CustomAppBar(
-          onLeadingPressed: () => scaffoldKey.currentState?.openDrawer(),
-        ),
-        drawer: Drawer(
-          backgroundColor: Colors.black,
-          child: ListView(
-            children: [
-              ListTile(
-                leading: const Icon(Icons.close, color: Colors.red),
-                title: const Text('Close', style: TextStyle(color: Colors.red)),
-                onTap: () => scaffoldKey.currentState?.closeDrawer(),
-              ),
-              const Divider(color: Colors.white),
-              ...cardList.map((card) {
-                return ListTile(
-                  trailing: Image.asset(card['image'] as String,
-                      width: 30, height: 30),
-                  title: Text(
-                    card['title'] as String,
-                    textDirection: TextDirection.rtl,
-                    style: const TextStyle(color: Colors.white),
-                  ),
-                  onTap: () {
-                    navigateWithSlideTransition(
-                      context,
-                      BaseScaffold(
-                        title: card['title'] as String,
-                        body: card['screen'] as Widget,
-                      ),
-                    );
-                    scaffoldKey.currentState?.closeDrawer();
-                  },
-                );
-              })
-            ],
-          ),
-        ),
-        body: Navigator(onGenerateRoute: (settings) {
-          return MaterialPageRoute(
-            builder: (context) => BaseScaffold(
-              title: HomeScreenTexts.navigationText,
-              body: body,
-              bottomNavigationBar: bottomNavigationBar,
-              showOrangeBar: false,
-            ),
-          );
-        })
-        );
-  }
-}
-
-
