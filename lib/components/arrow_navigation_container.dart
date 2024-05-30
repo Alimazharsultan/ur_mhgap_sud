1cc3200d762ef23f520bb575e8e5d47b875fee93
Ali Mazhar <alimazharsultan@gmail.com>
2024-05-30 16:14:44 +0500

    arrow navigation bar text fix

--- lib/components/arrow_navigation_container.dart
+++ lib/components/arrow_navigation_container.dart
@@ -18,69 +18,74 @@ class ArrowNavigationContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: navigationBarColor,
-      height: 50,
-      child: Stack(
-        children: <Widget>[
-          // Back arrow on the right
-          if (showBackButton)
-            Align(
-              alignment: Alignment.centerRight,
-              child: IconButton(
-                icon: const Icon(Icons.arrow_forward_ios,
-                    color: sideBarTextColor),
-                onPressed: () => {Navigator.pop(context)},
-              ),
-            ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+        color: navigationBarColor,
+        height: 50,
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(
+            minHeight: 50.0,
+          ),
+          child: Stack(
             children: <Widget>[
-              
-              Container(
-                alignment: Alignment.centerLeft,
-                width: 150,
-                child: Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: BaseTextComponent(
-                      text: text,
-                      color: sideBarTextColor,
-                    )),
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  height: 50, // Adjust the height to fit your design needs
-                  width: 2, // Adjust the width for the thickness of the divider
-                  color: sideBarTextColor,
-                ),
-              ),
-              Container(
-                alignment: Alignment.center,
-                width: 150,
-                child: const Padding(
-                  padding: EdgeInsets.only(left: 20),
-                  child: BaseTextComponent(
-                    text: 'نشہ آور اشیاء کا استعمال',
-                    color: sideBarTextColor,
-                    fontSize: normalTextSize,
+              // Back arrow on the right
+              if (showBackButton)
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: IconButton(
+                    icon: const Icon(Icons.arrow_forward_ios,
+                        color: sideBarTextColor),
+                    onPressed: () => {Navigator.pop(context)},
                   ),
                 ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.centerRight,
+                    width: 160,
+                    child: Padding(
+                        padding: const EdgeInsets.only(right: 30),
+                        child: BaseTextComponent(
+                          text: text,
+                          color: sideBarTextColor,
+                        )),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: Container(
+                      height: 50, // Adjust the height to fit your design needs
+                      width:
+                          2, // Adjust the width for the thickness of the divider
+                      color: sideBarTextColor,
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    width: 160,
+                    child: const Padding(
+                      padding: EdgeInsets.only(left: 0),
+                      child: BaseTextComponent(
+                        text: 'نشہ آور اشیاء کا استعمال',
+                        color: sideBarTextColor,
+                        fontSize: normalTextSize,
+                      ),
+                    ),
+                  ),
+                ],
               ),
+              // Eye icon on the left
+              if (showBackButton)
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: IconButton(
+                    icon: const Icon(Icons.remove_red_eye,
+                        color: sideBarTextColor),
+                    onPressed: () {
+                      navigateHomeWithSlideTransition(context);
+                    },
+                  ),
+                ),
             ],
           ),
-          // Eye icon on the left
-          if (showBackButton)
-            Align(
-              alignment: Alignment.centerLeft,
-              child: IconButton(
-                icon: const Icon(Icons.remove_red_eye, color: sideBarTextColor),
-                onPressed: () {
-                  navigateHomeWithSlideTransition(context);
-                },
-              ),
-            ),
-        ],
-      ),
-    );
+        ));
   }
 }
